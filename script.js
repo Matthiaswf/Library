1015f8ef13bfe5048e57337f4db81ac5ba105d4d
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2024-03-25 14:19:08 +0900

    Books are now created and added to the Library

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 let library = [];
 
-function Book() {
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
     
+    library.push(this);
 }
+
+let book1 = new Book('book1', 'me',  '12', 'not read')
+let book2 = new Book('book2', 'alsome', '13', 'read')
+console.log(library);
