bf66b0e3d6d4694395a405d0d18ab547ab35a0cf
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2024-03-26 15:22:38 +0900

    Button creates a div per book and the info displays

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const container = document.querySelector('.container');
+let bookList = document.createElement('div');
+bookList.classList.add('bookList');
+container.appendChild(bookList);
+
 let library = [];
 
 function Book(title, author, pages, read) {
@@ -6,9 +11,25 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
     
+    this.info = function() {
+
+        return `${this.title}, by ${this.author}, ${this.pages} pages, ${this.read}.`;
+    }
+
     library.push(this);
 }
 
 let book1 = new Book('book1', 'me',  '12', 'not read')
 let book2 = new Book('book2', 'alsome', '13', 'read')
 console.log(library);
+
+const displayButton = document.querySelector('.button');
+displayButton.addEventListener('click', () => {
+    
+library.forEach((book) => {
+
+    let bookInfo = document.createElement('div');
+    bookList.appendChild(bookInfo);
+    bookInfo.textContent =  book.info();
+})         
+});
