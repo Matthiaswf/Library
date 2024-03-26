1c9146f0827a19e631eba6b63da114ac3a33e5a5
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2024-03-26 15:54:09 +0900

    Created a form that submits input data into the library array

--- script.js
+++ script.js
@@ -19,6 +19,20 @@ function Book(title, author, pages, read) {
     library.push(this);
 }
 
+function BookFromForm(title, author, pages, read) {
+    this.title = document.getElementById('title').value;
+    this.author = document.getElementById('author').value;
+    this.pages = document.getElementById('pages').value;
+    this.read = document.getElementById('read').value;
+    
+    this.info = function() {
+
+        return `${this.title}, by ${this.author}, ${this.pages} pages, ${this.read}.`;
+    }
+
+    library.push(this);
+}
+
 let book1 = new Book('book1', 'me',  '12', 'not read')
 let book2 = new Book('book2', 'alsome', '13', 'read')
 console.log(library);
